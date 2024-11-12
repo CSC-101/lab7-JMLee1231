45b8556169506922dd6c5b276e29d50a041b3cec
JMLee1231 <jlee1231@calpoly.edu>
2024-11-11 23:20:04 -0800

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,1 +1,10 @@
 
+#The purpose of the function is to convert a number input into a string
+#Input is a string
+#output is a float
+def str_to_float(str1:str)->[float]:
+    try:
+        return float(str1)
+
+    except ValueError:
+        return None
